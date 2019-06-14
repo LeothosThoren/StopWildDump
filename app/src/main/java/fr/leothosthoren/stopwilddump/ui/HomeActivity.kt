9f49d39d4bbc1932aa992a716652d0f8608c549a
LeothosThoren <soussou.med@live.fr>
2019-06-14 18:47:06 +0200

    Create Stop Wild Dump project

---
+++ app/src/main/java/fr/leothosthoren/stopwilddump/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package fr.leothosthoren.stopwilddump.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fr.leothosthoren.stopwilddump.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/fr/leothosthoren/stopwilddump/ui/ReportWildDumpActivity.kt
@@ -0,0 +1,13 @@
+package fr.leothosthoren.stopwilddump.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fr.leothosthoren.stopwilddump.R
+
+class ReportWildDumpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_wild_dump)
+    }
+}
